89b764330125b2de4c77c3424d75616e31d68290
JhonatasFelipe76 <jhonatasestudos571@gmail.com>
2026-09-07 16:54:54 +0100

    Primeiro commit

---
+++ andante-com-fe/src/main/java/com/github/jhonatafelipe76/andantecomfe/AndanteComFeApplication.java
@@ -0,0 +1,13 @@
+package com.github.jhonatafelipe76.andantecomfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndanteComFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndanteComFeApplication.class, args);
+	}
+
+}
